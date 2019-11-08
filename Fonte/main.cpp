9a0d5da78faf59459430a97cbacc44715bf46354
juninhocb <juninhocb@hotmail.com>
2019-11-07 22:50:35 -0300

    ATT

--- Fonte/main.cpp
+++ Fonte/main.cpp
@@ -1,18 +1,38 @@
 
 #include "thinger/thinger.h"
+#include <string.h>
 
+#define DEBUG true
 #define USER_ID             "juninhocb"
 #define DEVICE_ID           "Raspberry"
 #define DEVICE_CREDENTIAL   "Teste12345"
+
+char = tensaoBateria[50]; 
 
 int main(int argc, char *argv[])
 {
     thinger_device thing(USER_ID, DEVICE_ID, DEVICE_CREDENTIAL);
 
-    // define thing resources here. i.e, this is a sum example
-    thing["sum"] = [](pson& in, pson& out){
-        out["result"] = (int) in["value1"] - (int) in["value2"];
-    };
+
+    //Serial.begin(9600);
+
+    /* Comandos para o bluetooh
+
+    tensaoBateria = sendData("AT+RV", 1000, DEBUG);  //tem que ver se a função retorna string	
+   
+ 
+    */
+
+    /* Enviando os dados para o thinger io
+
+    thing["Dados do Veículo"] >> [](pson& out){
+
+    out["Tensão da Bateria"] = tensaoBateria; //envia para o thinger io a tensão da bateria    
+	
+    }
+
+
+    */
 
     thing.start();
     return 0;
